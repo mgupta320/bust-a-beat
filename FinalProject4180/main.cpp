edb7ddd846d1f94fd48378e9f09621b25bbabd94
jho14 <jho67@gatech.edu>
2022-04-27 02:13:40 -0400

    loading song works

--- FinalProject4180/main.cpp
+++ FinalProject4180/main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-#include "Speaker.h"
 #include "uLCD_4DGL.h"
 #include "SDFileSystem.h"
 #include "PinDetect.h"
@@ -13,11 +12,6 @@
 
 //Volume level
 BusOut myleds(LED1, LED2, LED3, LED4);
-
-//Frequency one of song
-Speaker speaker1(p21);
-//Frequency two of song
-Speaker speaker2(p21);
 
 //Help guide recording if recording 
 //If not recording then show notes being played
@@ -33,6 +27,8 @@ PinDetect stringsPB(p16);
 
 //Red LEDs to show current instrument
 BusOut redLEDs(p22, p23, p24);
+//To show load or save
+DigitalOut sdLED(p29);
 
 // ======= INIT FOR TOUCH PAD =======
 // Create the interrupt receiver object on pin 26
@@ -61,7 +57,6 @@ RawSerial bluemod(p13,p14);
 
 //Threads
 Thread thread2;
-Thread thread3;
 Thread thread4;
 Thread thread5;
 
@@ -79,8 +74,8 @@ enum playback{PAUSE, PLAY};
 //Check how many places change state -> be sure to add mutex
 string currInstr = "piano";
 volatile int currState = STARTING;
-volatile int volume = 1.0;
-volatile int playback = PLAY;
+volatile float volume = 0.5;
+volatile int currPlayback = PAUSE;
 
 volatile int gridPosX = 0;
 volatile int gridPosY = 0;
@@ -123,17 +118,23 @@ void fallInterrupt() {
 
 void piano_hit_callback (void){  
     redLEDs.write(0x04);
-    currInstr = "piano";
+    currInstr_mutex.lock();
+        currInstr = "piano";
+    currInstr_mutex.unlock();
 }
 
 void synth_hit_callback (void){  
     redLEDs.write(0x02);
-    currInstr = "synth";
+    currInstr_mutex.lock();
+        currInstr = "synth";
+    currInstr_mutex.unlock();
 }
 
 void strings_hit_callback (void){  
     redLEDs.write(0x01);
-    currInstr = "strings";
+    currInstr_mutex.lock();
+        currInstr = "strings";
+    currInstr_mutex.unlock();
 }
 
 void lcdThread() {
@@ -160,30 +161,20 @@ void lcdThread() {
     }
 }
 
-void beatThread() {
-    while(1) {
-//        if (currState == PLAYING) {
-//            
-//        }
-        Thread::wait(1000.0*0.2);
-    }
-}
-
 void noteThread() {
     pc.printf("in thread");
     while(1) {
         pc.printf("in while");
-        currState = PLAYING;
         if (currState == PLAYING) {
             pc.printf("state is playing");
             FILE * wav_file; //increase PWM clock rate for audio
-                while(true) {
+                while(currState == PLAYING) {
                     for (int i = 0; i < 8; i++) {
                         currInstr_mutex.lock();
                         files[i] = "/sd/BABNotes/" + currInstr + "/" + currInstr + "-0" + notes[i] + ".wav";
                         currInstr_mutex.unlock();
                         if (beats[i] == "1") {
-                            mySpeaker.PlaySong(note,duration,0.001);
+                            mySpeaker.PlaySong(note,duration,volume/500.0);
                         }
                         char * tab2 = new char [files[i].length()+1];
                         strcpy (tab2, files[i].c_str());
@@ -206,32 +197,81 @@ void blueToothThread() {
                 bnum = bluemod.getc(); //button number
                 bhit = bluemod.getc(); //1=hit, 0=release
                 if (bluemod.getc()==char(~('!' + 'B' + bnum + bhit))) {
+                    pc.printf("inside bluetooth");
                     switch (bnum) {
                         case '1': //Playback Start
-                            playback = PLAY;
+                        {   
+                            pc.printf("start");
+                            currPlayback = PLAY;
                             break;   
+                        }
                         case '2': //Playback Stop
-                            playback = PAUSE;
+                        {
+                            pc.printf("stop");
+                            currPlayback = PAUSE;
                             break;
+                        }
                         case '3': //Save recording
+                        {
+                            pc.printf("save");
                             currState = SAVING;
                             break;
+                        }
                         case '4': //Load recording
+                        {
+                            pc.printf("load");
+                            string inputString;
                             currState = LOADING;
+                            pc.printf("Inside");
+                            FILE *fp = fopen("/sd/BABNotes/song.txt", "r");
+                            if(fp == NULL) {
+                                pc.printf("failed to open file");
+                            } else {
+                                int i = 0;
+                                while (fscanf(fp,"%s", inputString)!= EOF) //reads in a string delineated by white space
+                                { 
+                                    if (i < 8) {
+                                        notes[i] = inputString.c_str();
+                                    } else if (i < 15) {
+                                        beats[i-8] = inputString.c_str();
+                                    } 
+                                    i++;
+                                }
+                            }
+                            fclose(fp);
+                            
+                            for (int i = 0; i < 8; i++) {
+                                pc.printf(notes[i].c_str());
+                            }
+                            pc.printf("\n");
+                            for (int i = 0; i < 8; i++) {
+                                pc.printf(beats[i].c_str());
+                            }
+                            pc.printf("\n");
+                            currState = IDLE;
                             break;
+                        }
                         case '5': //Volume Up
-                            volume = std::min(1.0, volume + 0.1);
+                        {
+                            pc.printf("Up");
+                            pc.printf("Before %f", volume);
+                            volume = std::min(1.0, volume + 0.05);
                             break;
+                        }
                         case '6': //Volume Down
-                            volume = std::max(0.0, volume - 0.1);
+                        {
+                            pc.printf("Down");
+                            pc.printf("Before %f", volume);
+                            volume = std::max(0.0, volume - 0.05);
                             break;
+                        }
                         default:
                             break;
                     }                    
                 }
             }
         }
-        if (volume >= 1) {
+        if (volume >= 1.0) {
             myleds.write(0x0F);    
         } else if (volume >= 0.75) {
             myleds.write(0x07);    
@@ -241,9 +281,7 @@ void blueToothThread() {
             myleds.write(0x01); 
         } else {
             myleds.write(0x00);
-        }
-        myleds.write(0x0F);  
-        Thread::wait(1000.0*0.2);
+        }  
     }
 }
 
@@ -253,7 +291,7 @@ void init() {
     stringsPB.mode(PullUp); 
     interrupt.mode(PullUp);
     wait(0.01);
-    pc.printf("I am here\n");
+
     pianoPB.attach_deasserted(&piano_hit_callback);
     synthPB.attach_deasserted(&synth_hit_callback);
     stringsPB.attach_deasserted(&strings_hit_callback);
@@ -263,44 +301,21 @@ void init() {
     synthPB.setSampleFrequency();
     stringsPB.setSampleFrequency();
     
-    string inputString;
-    FILE *fp = fopen("/sd/BABNotes/song.txt", "r");
-    if(fp == NULL) {
-        pc.printf("failed to open file");
-    } else {
-        int i = 0;
-        while (fscanf(fp,"%s", inputString)!= EOF) //reads in a string delineated by white space
-        { 
-            if (i < 8) {
-                notes[i] = inputString.c_str();
-            } else if (i < 15) {
-                beats[i-8] = inputString.c_str();
-            } 
-            i++;
-        }
-    }
-    fclose(fp);
-    
-    for (int i = 0; i < 8; i++) {
-        pc.printf(notes[i].c_str());
-    }
-    pc.printf("\n");
-    for (int i = 0; i < 8; i++) {
-        pc.printf(beats[i].c_str());
-    }
-    pc.printf("\n");
+    redLEDs.write(0x04);
     
     thread2.start(lcdThread);
-    thread3.start(beatThread);
     thread4.start(noteThread);
     thread5.start(blueToothThread);
 }
 
 int main() {
     init();
-    //Touchpad
+    wait(2);
     currState = IDLE;
     while(1) {
-        wait(0.2);
+        if (currState == LOADING || currState == SAVING) sdLED = 1;
+        else sdLED = 0;
+        if (currState == IDLE && currPlayback == PLAY) currState = PLAYING;
+        Thread::wait(1000.0*0.2);
     }
 }
